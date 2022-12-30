a2429343f3d504cf7d91dbf9390e9ef08428eb74
Amrutapisal30 <104000771+Amrutapisal30@users.noreply.github.com>
2022-12-30 11:34:54 +0530

    Add All files

---
+++ C Programs/arearect.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int l,b,area;
+	printf("\n enter l= ");
+	scanf("%d",&l);
+	printf("\n enter b= ");
+	scanf("%d",&b);	
+	area=l*b;
+	printf("\n area of rectangle=%d",area);
+}
